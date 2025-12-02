92586496b8a4c32c08a628009de1a4ac96546ec3
lauhgabrielle14 <laurenfogaca_dev@hotmail.com>
2025-12-02 13:52:27 -0300

    :sparkles: Etapa Rotas

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -125,7 +125,7 @@ function Navbar({ menuState, onMenuToggle, onMenuClose }: Readonly<NavbarProps>)
                             <SearchForm />
                         </div>
                         
-                        {/* Links de navegação mobile */}
+                        {/* Links de navegação mobile OK */}
                         <Link to='/home' onClick={handleMenuClose} className="py-2 text-white hover:text-gray-300">
                             Home
                         </Link>
